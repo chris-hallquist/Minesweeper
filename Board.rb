e35eeb4e59d9c5668f0cb0a31ec66a60b2d730c9
App Academy Student <student@appacademy.io>
2013-11-25 15:52:12 -0800

    Committing weird dysfunctional code so Flarnie can study it

--- Board.rb
+++ Board.rb
@@ -1,4 +1,5 @@
 require_relative 'Tile'
+require 'debugger'
 
 class Board
 
@@ -15,25 +16,32 @@ class Board
 
   def initialize
     @tiles = Array.new(9) { Array.new(9) { Tile.new } }
-    populate_neighbors
+
     mine_board
   end
 
   def populate_neighbors
-    @tiles.each_index do |row|
-      @tiles[row].each_with_index do |tile, column|
-        neighbors_coords(row, column).each do |coords|
-          tile.neighbors << @tiles[row][column]
+    (0..8).each do |row|
+      (0..8).each do |column|
+
+        neighbors = neighbors_coords(row, column)
+
+        neighbors.each do |coords|
+          p [coords[0], coords[1]]
+          @tiles[row][column].neighbors << @tiles[coords[0]][coords[1]]
         end
       end
     end
+    return nil
   end
 
   def neighbors_coords(x,y)
     neighbors = []
 
     DIFFS.each do |coord|
-      neighbors << [x + coord.first, y + coord.last]
+      _x = x + coord.first
+      _y = y + coord.last
+      neighbors << [_x,_y] unless _x < 0 || _y < 0 || _x > 8 || _y > 8
     end
 
     neighbors
@@ -49,15 +57,32 @@ class Board
     end
   end
 
+  def _display
+    @tiles.each do |row|
+      row.each do |tile|
+        if tile.bombed && !tile.revealed
+          print "* "
+        elsif !tile.revealed
+          print "+ "
+        else
+          print "_ "
+        end
+      end
+      puts
+    end
+  end
+
   def display
     @tiles.each do |row|
       row.each do |tile|
         if tile.flagged
           print "F "
-        elsif !tile.revealed
-          print "* "
-        else
+        elsif tile.revealed && tile.neighbor_bomb_count > 0
+          print "#{tile.neighbor_bomb_count} "
+        elsif tile.revealed
           print "_ "
+        else
+          print "* "
         end
       end
       puts
@@ -67,4 +92,5 @@ end
 
 def test
   board = Board.new
+  board.display
 end
--- tile.rb
+++ tile.rb
@@ -1,16 +1,28 @@
 class Tile
-  attr_accessor :bombed, :flagged, :revealed, :neighbors
+  attr_accessor :bombed, :flagged, :revealed, :neighbors, :count_cache
 
   def initialize
     @bombed = false
     @flagged = false
     @revealed = false
     @neighbors = []
+    @count_cache = nil
   end
 
   def reveal
+    return :flagged if @flagged
+    @revealed = true
+    return :bomb if @bombed
+    return neighbor_bomb_count
   end
 
-  def neighbor_bomb_count
+  def neighbor_bomb_count(neighbors)
+    unless @count_cache
+      @count_cache = neighbors.inject(0) do |acumulator, neighbor|
+        acumulator + 1 if neighbor.bombed
+      end
+      @count_cache ||= 0
+    end
+    @count_cache
   end
 end
